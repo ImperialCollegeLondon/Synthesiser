ab97d7da5c745dd241aea12f873f8ec0b7972e32
ep1616 <ep1616@skbl-422-013.ph.ic.ac.uk>
2018-11-14 14:59:07 +0000

    UART testing, not working

--- UART.asm
+++ UART.asm
@@ -1,6 +1,6 @@
 #include p18f87k22.inc
 
-    global  UART_Setup, UART_Transmit_Message
+    global  UART_Setup, UART_Receive_Message
 
 acs0    udata_acs	    ; named variables in access ram
 UART_counter res 1	    ; reserve 1 byte for variable UART_counter
@@ -9,30 +9,27 @@ UART    code
     
 UART_Setup
     bsf	    RCSTA1, SPEN    ; enable
-    bcf	    TXSTA1, SYNC    ; synchronous
-    bcf	    TXSTA1, BRGH    ; slow speed
-    bsf	    TXSTA1, TXEN    ; enable transmit
-    bcf	    BAUDCON1, BRG16 ; 8-bit generator only
-    movlw   .103	    ; gives 9600 Baud rate (actually 9615)
+    bcf	    TXSTA1, SYNC    ; synchronous  - asynchronous
+    bcf	    TXSTA1, BRGH    ; slow speed    - BRGH low	
+    bcf	    BAUDCON1, BRG16 ; 8-bit generator only 
+    movlw   .31		    ; gives 31250 Baud rate
     movwf   SPBRG1
-    bsf	    TRISC, TX1	    ; TX1 pin as output
+    bsf	    TRISC, RX1	    ; RX1 pin as input
     return
 
-UART_Transmit_Message	    ; Message stored at FSR2, length stored in W
+UART_Receive_Message	    ; Message stored at FSR2, length stored in W
     movwf   UART_counter
 UART_Loop_message
     movf    POSTINC2, W
-    call    UART_Transmit_Byte
+    call    UART_Receive_Byte
     decfsz  UART_counter
     bra	    UART_Loop_message
     return
 
-UART_Transmit_Byte	    ; Transmits byte stored in W
-    btfss   PIR1,TX1IF	    ; TX1IF is set when TXREG1 is empty
-    bra	    UART_Transmit_Byte
-    movwf   TXREG1
+UART_Receive_Byte	    ; Transmits byte stored in W
+    btfss   PIR1,RC1IF	    ; RC1IF is set when RCREG1 is full (cleared when read)
+    bra	    UART_Receive_Byte
+    movwf   RCREG1
     return
 
     end
-
-
